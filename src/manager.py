8c27cf19af3249da3459606f7d7cc0a9c1b37533
xuwang <xuwangzihao@megvii.com>
2018-05-04 11:22:26 +0800

    system online
    ```
    cd logs
    tail -f *
    ```
    
    ```
    cd src
    python manager.py runserver
    ```
    
    访问
    127.0.0.1:22232/api/audits/1
    
    观察celery log的变化

--- src/manager.py
+++ src/manager.py
@@ -1,7 +1,4 @@
 
-
-
-
 from flask.ext.script import Server, Shell, Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
 
@@ -9,19 +6,38 @@ from my_app import app
 from my_app.foundation import db
 
 
-
-
-
 manager = Manager(app)
 migrate = Migrate(app, db)
 
-manager.add_command("runserver", Server('0.0.0.0', port=22222))
+manager.add_command("runserver", Server('0.0.0.0', port=22232))
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def dropall():
+    "Drops all database tables"
+    if prompt_bool("Are you sure ? You will lose all your data !"):
+        db.drop_all()
 
 
+@manager.command
+def createall():
+    "Creates database tables"
+    db.create_all()
+    db.session.commit()
 
 
+@manager.command
+def resetall():
+    dropall()
+    createall()
 
 
+if __name__ == "__main__":
 
+    import logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+    manager.run()
